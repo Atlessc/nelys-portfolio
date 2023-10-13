44ba04858e01982a933159e25fb518575da0d984
Tyler Smith <tylersmith4596@gmail.com>
2023-10-13 13:36:06 -0700

    submitting changes going to setup website using nextJS instead

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,84 @@
+import { useEffect, useState } from 'react';
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  environment: import.meta.env.VITE_CONTENTFUL_ENVIRONMENT,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+});
+
 export default function Home() {
+
+
+    const [posts, setPosts] = useState(null);
+
+    const client = contentful.createClient({
+      space: '<space_id>',
+      environment: '<environment_id>', // defaults to 'master' if not set
+      accessToken: '<content_delivery_api_key>'
+    })
+    
+    client.getEntries()
+    .then((response) => console.log(response.items))
+    .catch(console.error)
+
+    // useEffect(() => {
+    //   window
+    //     .fetch(`https://graphql.contentful.com/content/v1/spaces/${client.space}/`, {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         // Authenticate the request
+    //         Authorization: `Bearer ${client.accessToken}`,
+    //       },
+    //       // send the GraphQL query
+    //       body: JSON.stringify({ query }),
+    //     })
+    //     .then((response) => response.json())
+    //     .then(({ data, errors }) => {
+    //       if (errors) {
+    //         console.error(errors);
+    //       }
+  
+    //       // rerender the entire component with new data
+    //       console.log(data.pageCollection.items[0]);
+    //     });
+    // }, []);
   return (
     <>
       <div className="home">
         <div className="home-content">
           <h1 className="home-title">[NAME]</h1>
           <h2 className="home-subtitle">[SUBTITLE]</h2>
-          <div className="home-links">
-            <p>x</p>
-            <p>x</p>
+          <div className="home-gallery">
+          {posts ? (
+        posts.map((post) => (
+          <div key={post.sys.id} className="post-thumbnail">
+            <Link to={`/projects/${post.fields.postUuid}`}>
+              <img src={post.fields.thumbnail.fields.file.url} alt={post.fields.title} />
+              <h2>{post.fields.title}</h2>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
             </div>
         </div>
       </div>
     </>
   )
 }
+
+const query = `#graphql
+{
+  pageCollection {
+    items {
+      title
+      logo {
+        url
+      }
+    }
+  }
+}
+`
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,15 +1,45 @@
-export default function Project() {
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { createClient } from 'contentful';
+
+// Create a client to access Contentful
+const client = createClient({
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+});
+
+function Project() {
+  const { id } = useParams();
+  const [project, setProject] = useState(null);
+
+  useEffect(() => {
+    // Fetch the post data from Contentful
+    client.getEntry(id)
+      .then((entry) => setPost(entry.fields))
+      .catch(console.error);
+  }, [id]);
+
+  // Render the post data
   return (
-    <>
-      <div className="project">
-        <div className="project-content">
-          <h1 className="project-title">Project</h1>
-          <div className="project-links">
-            <p>x</p>
-            <p>x</p>
+    <div className="post-container">
+      {project ? (
+        <>
+          <h1 className="post-title">{project.title}</h1>
+          <p className="post-description">{project.description}</p>
+          {/* Render each image in the post */}
+          {project.image.map((img, index) => (
+            <div key={index} className="post-img-container">
+              <img src={img.fields.file.url} alt={img.fields.description || ''} className="post-img" />
+              <h2 className="post-img-title">{img.fields.title}</h2>
+              <p className="post-img-description">{img.fields.description}</p>
             </div>
-        </div>
-      </div>
-    </>
-  )
+          ))}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
+
+export default Project;
